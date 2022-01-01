59f85caf45c83b74a132125c06850a5d9640641b
liuyangxing <ningwang19940725@163.com>
2022-01-01 20:28:30 +0800

    lyx-dell-0927

--- src/main/java/com/ke/mall/MallApplication.java
+++ src/main/java/com/ke/mall/MallApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MallApplication {
 
     public static void main(String[] args) {
+        // lyxtest
         SpringApplication.run(MallApplication.class, args);
     }
 
