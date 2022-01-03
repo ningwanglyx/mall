a6f516d7544413fa00517d8e0a298d7890eee748
liuyangxing <ningwang19940725@163.com>
2022-01-03 18:34:07 +0800

    lyx-test

--- src/test/java/com/ke/mall/MallApplicationTests.java
+++ src/test/java/com/ke/mall/MallApplicationTests.java
@@ -24,6 +24,7 @@ public class MallApplicationTests {
     //@Autowired
     //private TestRestTemplate restTemplate;
     // tst
+    // test git merge
 
     @Test
     public void contextLoads() {
